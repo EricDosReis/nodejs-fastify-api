da138410ab7ea31e399c68740c26e4fe754256f8
EricDosReis <ericdosreis.personal@gmail.com>
2024-02-07 11:16:41 -0300

    feat: add env variables and validate if them exist

---
+++ env/index.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+const envSchema = z.object({
+  DATABASE_URL: z.string(),
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  PORT: z.number().default(3333),
+});
+
+const parsedEnvSchema = envSchema.safeParse(process.env);
+
+if (parsedEnvSchema.success === false) {
+  const errorMessage = 'Invalid environment variables!';
+
+  console.error(errorMessage, parsedEnvSchema.error.format());
+
+  throw new Error(errorMessage);
+}
+
+export const env = parsedEnvSchema.data;
