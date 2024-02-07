c5ebbdc23835d1386ba36b2f055b8311fe456053
EricDosReis <ericdosreis.personal@gmail.com>
2024-02-07 15:28:23 -0300

    feat: create list and detail transaction end points

--- src/routes/transactions/transactions.ts
+++ src/routes/transactions/transactions.ts
@@ -2,9 +2,23 @@ import type { FastifyInstance } from 'fastify';
 import { randomUUID } from 'node:crypto';
 
 import { knex } from '../../database';
-import { createBodySchema } from './schemas';
+import { createBodySchema, getTransactionParamsSchema } from './schemas';
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select();
+
+    return { transactions };
+  });
+
+  app.get('/:id', async request => {
+    const { id } = getTransactionParamsSchema.parse(request.params);
+
+    const transaction = await knex('transactions').where('id', id).first();
+
+    return { transaction };
+  });
+
   app.post('/', async (request, response) => {
     const { amount, title, type } = createBodySchema.parse(request.body);
 
